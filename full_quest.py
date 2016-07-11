3997405e343c19e1a0a90b297554e113131a6883
Andrey Medvedev <andr.medv.spb@gmail.com>
2016-07-11 22:55:36 +0300

    Remove comments

--- full_quest.py
+++ full_quest.py
@@ -125,7 +125,6 @@ def init_sounds(game_state):
 
 def REQ_QUEST_INIT(master, task, game_state):
     init_sounds(game_state)
-    # return True
     # close all boxes
     sl_controlls = master.getSimpleLeds(Devices.LOVECRAFT_DEVICE_NAME).get()
     sl_controlls[
@@ -215,16 +214,6 @@ def TABLE_CLOCK_UP(master):
         GLOBAL_VARIABLES.TABLE_CLOCK_VALUE = GLOBAL_VARIABLES.TABLE_CLOCK_VALUE - 12
 
     print("TABLE CLOCK VALUE: {}".format(GLOBAL_VARIABLES.TABLE_CLOCK_VALUE))
-
-    # sl_controlls = master.getSimpleLeds(Devices.LOVECRAFT_DEVICE_NAME).get()
-    # sl_controlls[DEVICES_TABLE.SL_TABLE_CLOCK_RING_OUT] = 1
-    #
-    # master.setSimpleLeds(Devices.LOVECRAFT_DEVICE_NAME, sl_controlls)
-    # time.sleep(1)
-    #
-    # sl_controlls[DEVICES_TABLE.SL_TABLE_CLOCK_RING_OUT] = 0
-    # master.setSimpleLeds(Devices.LOVECRAFT_DEVICE_NAME, sl_controlls)
-
 
 def AC_ADD_TABLE_CLOCK_RING_OUT(master, task, game_state):
     game_state.add_active_task_with_id(TASKS_IDS.TABLE_CLOCK_RING_OUT_ALWAYS)
